b68af92c9204fc782b3758f2e7e3e69b03bba154
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2022-03-18 00:15:46 +0100

    Update dateur.php

--- wssv/cedric/dateur.php
+++ wssv/cedric/dateur.php
@@ -14,10 +14,38 @@ elseif ($_GET['operation'] == 'previousDay'){
     $date = strtotime($_GET['lastDate']) - (60 * 60 * 24) ;
     $dateToShow = date("d-m-Y", $date);
 }
+elseif ($_GET['operation'] == 'previousMonth'){
+
+    
+    $date = strtotime($_GET['lastDate']) - (60 * 60 * 24) ;
+    $dateToShow = date("d-m-Y", $date);
+}
+elseif ($_GET['operation'] == 'previousYear'){
+
+    
+    $date = strtotime($_GET['lastDate']) - (60 * 60 * 24 * 365) ;
+    $dateToShow = date("d-m-Y", $date);
+}
 elseif ($_GET['operation'] == 'nextDay'){
 
     
     $date = strtotime($_GET['lastDate']) + (60 * 60 * 24); 
+    
+    $dateToShow = date("d-m-Y", $date);
+    
+}
+elseif ($_GET['operation'] == 'nextMonth'){
+
+    
+    $date = strtotime($_GET['lastDate']) + (60 * 60 * 24); 
+    
+    $dateToShow = date("d-m-Y", $date);
+    
+}
+elseif ($_GET['operation'] == 'nextYear'){
+
+    
+    $date = strtotime($_GET['lastDate']) + (60 * 60 * 24 * 365); 
     
     $dateToShow = date("d-m-Y", $date);
     
@@ -32,9 +60,13 @@ elseif ($_GET['operation'] == 'nextDay'){
     </head>
     <body>
     <main>
+        <button class="less"><a  href="dateur.php?operation=previousYear&lastDate=<?= $dateToShow ?>" >Année précédente</a></button>
+        <button class="less"><a  href="dateur.php?operation=previousMonth&lastDate=<?= $dateToShow ?>" >Mois précédent</a></button>
         <button class="less"><a  href="dateur.php?operation=previousDay&lastDate=<?= $dateToShow ?>" >Jour précédent</a></button>
         <span class="date"><?= $dateToShow ?></span>
         <button class="more"><a  href="dateur.php?operation=nextDay&lastDate=<?= $dateToShow ?>" >Jour suivant</a></button>
+        <button class="more"><a  href="dateur.php?operation=nextMonth&lastDate=<?= $dateToShow ?>" >Mois suivant</a></button>
+        <button class="more"><a  href="dateur.php?operation=nextYear&lastDate=<?= $dateToShow ?>" >Année suivante</a></button>
 
     </main>
     <footer>
